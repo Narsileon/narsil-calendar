33ff71277cf132143b1f2593468fb9963ee79839
Narsilien <rigaux.jonathan@gmail.com>
2024-08-18 13:14:10 +0200

    Add css

--- resources/js/Components/Calendar/FullCalendar.tsx
+++ resources/js/Components/Calendar/FullCalendar.tsx
@@ -1,3 +1,4 @@
+import "@narsil-calendar/../css/app.scss";
 import { CalendarOptions, DatesSetArg } from "@fullcalendar/core";
 import { forwardRef } from "react";
 import { usePage } from "@inertiajs/react";
@@ -10,6 +11,8 @@ import moment from "moment";
 import rrulePlugin from "@fullcalendar/rrule";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import useScreenStore from "@narsil-ui/Stores/screenStore";
+import ScrollArea from "@narsil-ui/Components/ScrollArea/ScrollArea";
+import * as React from "react";
 
 export interface FullCalendarProps extends CalendarOptions {}
 
@@ -18,16 +21,12 @@ const FullCalendar = forwardRef<Calendar, FullCalendarProps>(({ views, select, .
 
 	const locale = usePage<GlobalProps>().props.shared.localization.locale;
 
+	const [width, setWidth] = React.useState("100%");
+
 	const LOCAL_STORAGE_FULL_CALENDAR_DEFAULT_VIEW = "fullCalendarDefaultView";
 
-	const datesSet = (arg: DatesSetArg) => {
-		localStorage.setItem(LOCAL_STORAGE_FULL_CALENDAR_DEFAULT_VIEW, arg.view.calendar.getDate().toISOString());
-	};
-
-	const getWidth = () => {
-		if (isMobile && ref && "current" in ref && ref?.current) {
-			let view = ref.current.getApi().view.type;
-
+	const getWidth = (view: string) => {
+		if (isMobile) {
 			if (!view.includes("list") && !view.includes("Day")) {
 				return "200%";
 			}
@@ -36,87 +35,102 @@ const FullCalendar = forwardRef<Calendar, FullCalendarProps>(({ views, select, .
 		return "100%";
 	};
 
+	const datesSet = (arg: DatesSetArg) => {
+		setWidth(getWidth(arg.view.type));
+		console.log(arg.view.type);
+		localStorage.setItem(LOCAL_STORAGE_FULL_CALENDAR_DEFAULT_VIEW, arg.view.calendar.getDate().toISOString());
+	};
+
 	const plugins = [dayGridPlugin, interactionPlugin, listPlugin, rrulePlugin, timeGridPlugin];
 
 	return (
-		<Calendar
-			ref={ref}
-			allDaySlot={true}
-			datesSet={datesSet}
-			dayMaxEvents={true}
-			dayMaxEventRows={true}
-			editable={false}
-			eventMaxStack={10}
-			eventTimeFormat={{
-				hour: "2-digit",
-				minute: "2-digit",
-				hour12: locale == "en" ? true : false,
-			}}
-			firstDay={1}
-			headerToolbar={false}
-			height='100%'
-			initialDate={
-				localStorage.getItem(LOCAL_STORAGE_FULL_CALENDAR_DEFAULT_VIEW) !== null
-					? localStorage.getItem(LOCAL_STORAGE_FULL_CALENDAR_DEFAULT_VIEW)
-					: moment().toDate()
-			}
-			initialView='timeGridWeek'
-			locale={locale}
-			locales={allLocales}
-			longPressDelay={500}
-			nowIndicator={true}
-			plugins={plugins}
-			progressiveEventRendering={true}
-			schedulerLicenseKey='CC-Attribution-NonCommercial-NoDerivatives'
-			scrollTime='08:00'
-			scrollTimeReset={true}
-			slotMinTime='00:00'
-			slotMaxTime='24:00'
-			select={select}
-			selectMirror={true}
-			style={{
-				width: getWidth(),
-			}}
-			views={{
-				day: {
-					dayHeaderFormat: {
-						weekday: "long",
-					},
-					titleFormat: {
-						day: "numeric",
-						month: "long",
-						year: "numeric",
-					},
-				},
-				week: {
-					dayHeaderFormat: {
-						day: "2-digit",
-						weekday: isMobile ? "short" : "long",
-					},
-					titleFormat: {
-						day: "numeric",
-						month: "long",
-						year: "numeric",
-					},
-				},
-				month: {
-					dayHeaderFormat: {
-						weekday: isMobile ? "short" : "long",
-					},
-				},
-				timeGrid: {
-					slotLabelFormat: {
-						days: 1,
+		<ScrollArea
+			className='h-full w-full overflow-hidden rounded-md border'
+			orientation='horizontal'
+		>
+			<div
+				className='h-full'
+				style={{
+					width: width,
+				}}
+			>
+				<Calendar
+					ref={ref}
+					allDaySlot={true}
+					datesSet={datesSet}
+					dayMaxEvents={true}
+					dayMaxEventRows={true}
+					editable={false}
+					eventMaxStack={10}
+					eventTimeFormat={{
 						hour: "2-digit",
 						minute: "2-digit",
 						hour12: locale == "en" ? true : false,
-					},
-				},
-				...views,
-			}}
-			weekNumbers={false}
-			{...props}
-		/>
+					}}
+					firstDay={1}
+					headerToolbar={false}
+					height='100%'
+					initialDate={
+						localStorage.getItem(LOCAL_STORAGE_FULL_CALENDAR_DEFAULT_VIEW) !== null
+							? localStorage.getItem(LOCAL_STORAGE_FULL_CALENDAR_DEFAULT_VIEW)
+							: moment().toDate()
+					}
+					initialView='timeGridWeek'
+					locale={locale}
+					locales={allLocales}
+					longPressDelay={500}
+					nowIndicator={true}
+					plugins={plugins}
+					progressiveEventRendering={true}
+					schedulerLicenseKey='CC-Attribution-NonCommercial-NoDerivatives'
+					scrollTime='08:00'
+					scrollTimeReset={true}
+					slotMinTime='00:00'
+					slotMaxTime='24:00'
+					select={select}
+					selectMirror={true}
+					views={{
+						day: {
+							dayHeaderFormat: {
+								weekday: "long",
+							},
+							titleFormat: {
+								day: "numeric",
+								month: "long",
+								year: "numeric",
+							},
+						},
+						week: {
+							dayHeaderFormat: {
+								day: "2-digit",
+								weekday: isMobile ? "short" : "long",
+							},
+							titleFormat: {
+								day: "numeric",
+								month: "long",
+								year: "numeric",
+							},
+						},
+						month: {
+							dayHeaderFormat: {
+								weekday: isMobile ? "short" : "long",
+							},
+						},
+						timeGrid: {
+							slotLabelFormat: {
+								days: 1,
+								hour: "2-digit",
+								minute: "2-digit",
+								hour12: locale == "en" ? true : false,
+							},
+						},
+						...views,
+					}}
+					weekNumbers={false}
+					{...props}
+				/>
+			</div>
+		</ScrollArea>
 	);
 });
 
